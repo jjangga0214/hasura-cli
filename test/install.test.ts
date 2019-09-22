5658e666f1cba5f6bc44ada0a4c8d6862485673d
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-09-22 20:27:59 +0900

    test(version): test against current package version
    from package.json

--- test/install.test.ts
+++ test/install.test.ts
@@ -5,8 +5,11 @@ import { install } from '#/install'
 
 const exec = util.promisify(require('child_process').exec)
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { version } = require('../package.json')
+
 describe('install', () => {
-  it('install, exec, and check versions of every cli', async () => {
+  it('install, execute, and check version of cli', async () => {
     expect.hasAssertions()
 
     const askVersion = async (dest: string): Promise<string> => {
@@ -18,43 +21,32 @@ describe('install', () => {
         return stdout
       }
     }
+    const destDir = path.resolve(__dirname, 'tmp')
+    const dest = await install({
+      destDir,
+      fileName: `${Date.now()}-${version}`,
+      verbose: true,
+    })
 
-    const betas = [...Array(6).keys()]
-      .map(i => i + 1)
-      .map(i => `1.0.0-beta.${i}`)
-    const alphas = [...Array(45).keys()]
-      .map(i => i + 1)
-      .map(i => `1.0.0-alpha${i < 10 ? `0${i}` : i}`)
-    for (const version of [
-      // '1.0.0-beta.6',
-      // '1.0.0-beta.1',
-      // '1.0.0-alpha45',
-      // '1.0.0-alpha29',
-      // '1.0.0-alpha01',
-      ...betas,
-      ...alphas,
-    ]) {
-      const dest = await install({
-        version,
-        destDir: path.resolve(__dirname, 'tmp'),
-        fileName: `${Date.now()}-${version}`,
-        verbose: true,
-      })
+    const stdout = await askVersion(dest)
+    console.log(stdout)
 
-      const stdout = await askVersion(dest)
-      console.log(stdout)
-
-      try {
-        // When tag and printed version are same
-        expect(stdout).toContain(version)
-      } catch (err) {
-        // When tag and printed version are different
-        // e.g. '1.0.0-beta.1' -> '1.0.0-beta.01'
-        const adjustedVersion = `${version.slice(0, -1)}0${version.slice(-1)}`
-        // eslint-disable-next-line jest/no-try-expect
-        expect(stdout).toContain(adjustedVersion)
-      }
-      await fs.unlinkSync(dest)
+    try {
+      // When tag and printed version are same
+      expect(stdout).toContain(version)
+    } catch (err) {
+      // When tag and printed version are different
+      // e.g. '1.0.0-beta.1' -> '1.0.0-beta.01'
+      const adjustedVersion = `${version.slice(0, -1)}0${version.slice(-1)}`
+      // eslint-disable-next-line jest/no-try-expect
+      expect(stdout).toContain(adjustedVersion)
     }
-  }, 24000000)
+    fs.unlinkSync(dest)
+    try {
+      fs.rmdirSync(destDir)
+      // This error handling repects other parallel tests
+    } catch (err) {
+      console.error(err)
+    }
+  }, 45000)
 })
