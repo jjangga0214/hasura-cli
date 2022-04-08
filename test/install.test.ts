b3dd7efb2dbfd7c32ae4e07a922b524934d400b7
jjangga0214 <bnbcmindnpass@gmail.com>
2022-04-08 14:49:30 +0900

    chore(release): v2.2.2

--- test/install.test.ts
+++ test/install.test.ts
@@ -9,57 +9,61 @@ const exec = util.promisify(require('child_process').exec)
 const { version } = require('../package.json')
 
 describe('install', () => {
-  it('install, execute, and check version of cli', async () => {
-    expect.hasAssertions()
+  it(
+    'install, execute, and check version of cli',
+    async () => {
+      expect.hasAssertions()
 
-    const askVersion = async (dest: string): Promise<string> => {
-      try {
-        const { stdout, stderr } = await exec(
-          `${dest} version --skip-update-check`,
-        )
-        // Why stderr? REF: https://github.com/hasura/graphql-engine/issues/6998
-        if (version === '2.0.0-alpha.11') {
-          return stderr
+      const askVersion = async (dest: string): Promise<string> => {
+        try {
+          const { stdout, stderr } = await exec(
+            `${dest} version --skip-update-check`,
+          )
+          // Why stderr? REF: https://github.com/hasura/graphql-engine/issues/6998
+          if (version === '2.0.0-alpha.11') {
+            return stderr
+          }
+          return stdout
+        } catch (err) {
+          const { stdout } = await exec(`${dest} version`)
+          return stdout
         }
-        return stdout
-      } catch (err) {
-        const { stdout } = await exec(`${dest} version`)
-        return stdout
       }
-    }
-    const destDir = path.resolve(__dirname, 'tmp')
-    // eslint-disable-next-line jest/no-if
-    if (!fs.existsSync(destDir)) {
-      fs.mkdirSync(destDir)
-    }
-    // eslint-disable-next-line jest/no-if
-    if (process.platform === 'win32') {
-      fs.closeSync(fs.openSync(path.resolve(destDir, 'hasura'), 'w'))
-    }
-    const dest = await install({
-      destDir,
-      fileName: `${Date.now()}-${version}`,
-      verbose: true,
-    })
+      const destDir = path.resolve(__dirname, 'tmp')
+      // eslint-disable-next-line jest/no-if
+      if (!fs.existsSync(destDir)) {
+        fs.mkdirSync(destDir)
+      }
+      // eslint-disable-next-line jest/no-if
+      if (process.platform === 'win32') {
+        fs.closeSync(fs.openSync(path.resolve(destDir, 'hasura'), 'w'))
+      }
+      const dest = await install({
+        destDir,
+        fileName: `${Date.now()}-${version}`,
+        verbose: true,
+      })
 
-    const stdout = await askVersion(dest)
-    console.log(stdout)
-    try {
-      // When tag and printed version are same
-      expect(stdout).toContain(version)
-    } catch (err) {
-      // When tag and printed version are different
-      // e.g. '1.0.0-beta.1' -> '1.0.0-beta.01'
-      const adjustedVersion = `${version.slice(0, -1)}0${version.slice(-1)}`
-      // eslint-disable-next-line jest/no-try-expect
-      expect(stdout).toContain(adjustedVersion)
-    }
-    fs.unlinkSync(dest)
-    try {
-      fs.rmdirSync(destDir)
-      // This error handling repects other parallel tests
-    } catch (err) {
-      console.error(err)
-    }
-  }, 45000)
+      const stdout = await askVersion(dest)
+      console.log(stdout)
+      try {
+        // When tag and printed version are same
+        expect(stdout).toContain(version)
+      } catch (err) {
+        // When tag and printed version are different
+        // e.g. '1.0.0-beta.1' -> '1.0.0-beta.01'
+        const adjustedVersion = `${version.slice(0, -1)}0${version.slice(-1)}`
+        // eslint-disable-next-line jest/no-try-expect
+        expect(stdout).toContain(adjustedVersion)
+      }
+      fs.unlinkSync(dest)
+      try {
+        fs.rmdirSync(destDir)
+        // This error handling repects other parallel tests
+      } catch (err) {
+        console.error(err)
+      }
+    },
+    90 * 1000,
+  )
 })
