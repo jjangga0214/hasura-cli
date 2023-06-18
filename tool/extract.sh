1d096a393b1b35871fb6e1e618a9cceaf96161fe
永野峻輔 <peace.infi0407@gmail.com>
2023-06-18 09:23:38 +0900

    ci: when version not changed, skip bumping

--- tool/extract.sh
+++ tool/extract.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 WORK_DIR=$(cd "$(dirname "$0")" && pwd)
-VERSION=$(sed -n -e  '/github.com\/hasura\/graphql-engine\/cli/p' "${WORK_DIR}"/go.mod | cut -d ' ' -f3)
-
-npm version ${VERSION#v} --git-tag-version false
+VERSION_TEXT=$(sed -n -e  '/github.com\/hasura\/graphql-engine\/cli/p' "${WORK_DIR}"/go.mod | cut -d ' ' -f3)
+VERSION="${VERSION_TEXT#v}"
+CURRENT_VERSION=$(cat package.json | jq -r '.version')
+if [ "$VERSION" = "$CURRENT_VERSION" ]; then
+  echo "version not changed, so skip bumping version"
+  exit 0
+fi
+npm version $VERSION --git-tag-version false
